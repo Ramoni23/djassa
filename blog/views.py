d9d8a3b9b2bda2ec7899307f6b294c015b38945a
Ramoni22 <99032555+Ramoni22@users.noreply.github.com>
2023-10-23 13:47:49 +0200

    ajout du navbar

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+def home(request):
+    # afficher le model article sur la page d'accueil
+    list_articles = Article.objects.all()
+    context = {"list_articles":list_articles}
+    return render(request, "index.html", context)
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+# from django.urls import include
+from blog.views import home
+from django.conf.urls.static import static
+from . import settings
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    path('admin/', admin.site.urls),
+    # path('', include('blog.urls')), # affiche tous urls qui sont dans blog.urls
+    path('', home, name="home"), # affiche la fonction home
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
