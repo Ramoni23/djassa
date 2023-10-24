5ff3bd88632efe51a18b7c2684c2e783f58b0ab6
Ramoni22 <99032555+Ramoni22@users.noreply.github.com>
2023-10-24 15:31:43 +0200

    update de la page detail

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+# creation de la category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# creation des articles
 class Article(models.Model):
     title= models.CharField(max_length=120)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     desc= models.TextField()
     image = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
